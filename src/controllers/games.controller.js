e2ca39e9eb878b1b16f835d448866ad29e9da0e1
lujolfs <lucas.j.frank@gmail.com>
2023-02-08 22:02:47 -0300

    feat: create games function, removal of category

---
+++ src/controllers/games.controller.js
@@ -0,0 +1,24 @@
+import {db} from "../database/database.js";
+
+export async function create (req, res) {
+    const {name, image, stockTotal, pricePerDay} = req.body;
+
+    if (!name || stockTotal < 0 || pricePerDay < 0) {
+        return res.sendStatus(400)
+    }
+
+    try {
+        await db.query(
+            `
+            INSERT INTO games
+                (name, image, "stockTotal", "pricePerDay")
+            VALUES
+                ($1, $2, $3, $4);
+            `,
+            [name, image, stockTotal, pricePerDay]
+        );
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
