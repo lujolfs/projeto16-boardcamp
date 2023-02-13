e4de6d2c93a23afaf9435e9898f1412133267c5a
lujolfs <lucas.j.frank@gmail.com>
2023-02-13 15:38:54 -0300

    grunt

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { create, findAll, findById, update } from "../controllers/customers.controller.js";
-import { schemaValidateCustomer } from "../middlewares/schemaValidateCustomers.js";
+import { schemaValidateCustomer, schemaValidateCustomerUpdate } from "../middlewares/schemaValidateCustomers.js";
 
 const customersRouter = Router();
 
 customersRouter.post("/customers", schemaValidateCustomer, create);
 customersRouter.get("/customers", findAll);
 customersRouter.get("/customers/:id", findById);
-customersRouter.get("/customers/:id", update);
+customersRouter.post("/customers/:id", schemaValidateCustomerUpdate, update);
 
 export default customersRouter;
