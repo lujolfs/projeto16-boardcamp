5be076df58f8dcd698f7b433547ef93a39a2e29c
lujolfs <lucas.j.frank@gmail.com>
2023-02-13 15:38:42 -0300

    feat: finish schema for updating customers

--- src/middlewares/schemaValidateCustomers.js
+++ src/middlewares/schemaValidateCustomers.js
@@ -30,3 +30,43 @@ export async function schemaValidateCustomer (req, res, next) {
     next();
     return;
 }
+
+export async function schemaValidateCustomerUpdate (req, res, next) {
+    const customer = req.body;
+    const {id} = req.params;
+    const matchSearch = db.query(`SELECT * FROM customers WHERE cpf=$1;`, [customer.cpf]);
+    const idMatch = db.query (`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5;`, [customer.name, customer.phone, customer.cpf, customer.birthday, id])
+
+    const {error} = customerSchema.validate(customer, {abortEarly: false});
+
+    if (error) {
+        const errors = error.details.map((detail) => detail.message);
+        return res.status(400).send(errors);
+    }
+
+    try {
+        const {rows} = await db.query(`
+        SELECT COUNT
+            (*)
+        FROM
+            customers
+        WHERE
+            cpf=$1;`, [customer.cpf]);
+        if (rows[0].count !== "0") {
+            await matchSearch;
+            if (rows[0].id = id) {
+                await idMatch
+            } else {
+                return res.sendStatus(409)
+            }}
+        else {
+            return res.sendStatus(409)
+        }
+        } catch (error) {
+        return res.sendStatus(401);
+    }
+
+    req.customerObject = customer;
+    next();
+    return;
+}
